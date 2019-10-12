8a5c93e65ca97c2de40dbf6c9576980207088420
MarioCMFlys <mariocmflysmail@gmail.com>
2019-10-11 22:15:52 -0500

    Update version to 1.2.1

--- src/me/mariocmflys/nmc/C.java
+++ src/me/mariocmflys/nmc/C.java
@@ -1,7 +1,7 @@
 package me.mariocmflys.nmc;
 
 public class C {
-	public static final String VERSION = "1.2.0";
+	public static final String VERSION = "1.2.1";
 	public static final int LOGICAL_VERSION = 0;
 	
 	public static final String URL_RESOURCE_DOWNLOAD = "https://resources.download.minecraft.net";
